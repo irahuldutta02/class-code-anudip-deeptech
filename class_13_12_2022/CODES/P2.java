072ebc7f597d483d7b90f75cad56cc9bc711bff0
Rahul Dutta <rdtech2002@gmail.com>
2022-12-15 12:52:45 +0530

    .

---
+++ class_13_12_2022/CODES/P2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class P2{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input first number : ");
+		int a = sc.nextInt();
+		System.out.print("Input Second number : ");
+		int b = sc.nextInt();
+		System.out.println("Sum of "+a+" and "+b+" is = "+(a+b));
+		System.out.println("Substraction of "+a+" and "+b+" is = "+(a-b));
+		System.out.println("Multiplication of "+a+" and "+b+" is = "+(a*b));
+		System.out.println("Division of "+a+" and "+b+" is = "+(a/b));
+	}
+}
---
+++ class_13_12_2022/CODES/P3.java
@@ -0,0 +1,6 @@
+public class P3{
+	public static void main(String[] args){
+		//operators
+		
+	}
+}
