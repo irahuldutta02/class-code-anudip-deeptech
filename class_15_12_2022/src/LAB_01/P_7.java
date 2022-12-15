7f5daf5c39d6bd6136ecd243285975e6a676b69f
Rahul Dutta <rdtech2002@gmail.com>
2022-12-15 13:03:50 +0530

    .

---
+++ class_15_12_2022/src/LAB_01/P_7.java
@@ -0,0 +1,15 @@
+// factorial
+
+package LAB_01;
+
+public class P_7 {
+	public static void main(String args[]){  
+		  int i,fact=1; 
+		  int number=5;   
+		  for(i=1;i<=number;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+number+" is: "+fact);    
+		 }  
+
+}
