6f2916d534a78388117dcf988b62ab4c6de45fb0
Rahul Dutta <rdtech2002@gmail.com>
2022-12-15 11:22:06 +0530

    .

---
+++ lab_15_12_2022/src/_01/P_1.java
@@ -0,0 +1,18 @@
+package _01;
+
+public class P_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i; // counter variable
+		for (i=0; i<5;i++) {
+			System.out.println("Hello");
+		}
+		System.out.println();
+		for (i=5; i>0;i--) {
+			System.out.println("World");
+		}
+
+	}
+
+}
