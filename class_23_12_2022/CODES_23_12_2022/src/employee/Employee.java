be0a39c5ffe6821911317c594877809a6e770127
irahuldutta02 <rdtech2002@gmail.com>
2022-12-23 13:15:13 +0530

    .

---
+++ class_23_12_2022/CODES_23_12_2022/src/employee/Employee.java
@@ -0,0 +1,60 @@
+package employee;
+
+public class Employee {
+	long employeeId;
+	String employeeName;
+	String employeeAddress;
+	long employeePhone;
+	double basicSalary;
+	final double specialAllowance = 250.80;
+	final double hra = 1000.50;
+	
+	public Employee(long employeeId, String employeeName, String employeeAddress, long employeePhone) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeAddress = employeeAddress;
+		this.employeePhone = employeePhone;
+	}
+	
+	public double calculateSalary() {
+		double salary = (basicSalary +(basicSalary * specialAllowance/100)+(basicSalary*hra/100));
+		return salary;
+	}
+	
+	public void calculatetransportAllowance(){
+		double transportAllowance = ((10*basicSalary)/100);
+		System.out.println("| Transport Allowance: "+ transportAllowance);
+	}
+	
+	public void displayDetails(){
+		System.out.println("| Employee ID: "+ employeeId);
+		System.out.println("| Employee NAME: "+ employeeName);
+		System.out.println("| Employee Address: "+ employeeAddress);
+		System.out.println("| Employee Phone: "+ employeePhone);
+	}
+	
+}
+
+class Manager extends Employee{
+
+	public Manager(long employeeId, String employeeName, String employeeAddress, long employeePhone, double salary) {
+		super(employeeId, employeeName, employeeAddress, employeePhone);
+		this.basicSalary = salary;
+	}
+	
+	public void calculatetransportAllowance(){
+		double transportAllowance = ((15*basicSalary)/100);
+		System.out.println("| Transport Allowance: "+ transportAllowance);
+	}
+}
+
+class Trainee extends Employee{
+
+	public Trainee(long employeeId, String employeeName, String employeeAddress, long employeePhone, double salary) {
+		super(employeeId, employeeName, employeeAddress, employeePhone);
+		
+		this.basicSalary = salary;
+	}
+	
+}
