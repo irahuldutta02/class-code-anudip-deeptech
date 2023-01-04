fcf99da5b2e6dcf2fba88ee2f122a4678abe0dab
irahuldutta02 <rdtech2002@gmail.com>
2023-01-04 14:33:28 +0530

    .

--- 019_class_04_01_2023/019_CODES_04_01_2023/src/basic/CountVowel.java
+++ 019_class_04_01_2023/019_CODES_04_01_2023/src/basic/CountVowel.java
@@ -1,25 +1,28 @@
 package basic;
-public class CountVowel {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String str = "Rahul Dutta";
-		str = str.toLowerCase();
+import java.util.Scanner;
+
+public class CountVowel {
+	
+	public static int count(String str) {
 		int count = 0;
 
 		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) == 'a' || str.charAt(i) == 'e'
-				|| str.charAt(i) == 'i'
-				|| str.charAt(i) == 'o'
-				|| str.charAt(i) == 'u') {
-	
+			if ((str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
+					|| str.charAt(i) == 'u')||(str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I' || str.charAt(i) == 'O'
+					|| str.charAt(i) == 'U')) {
 				count++;
 			}
 		}
-		System.out.println(
-				"Total no of vowels in string are: " + count);
+		return count;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("String : ");
+		String str = sc.nextLine();
+		System.out.println("Total no of vowels in string are: " + count(str));
 	}
 
 }
-
-
